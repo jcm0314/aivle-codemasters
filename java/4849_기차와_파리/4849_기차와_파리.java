2ce4fcfe72b64ae5199b1a33c47559fa73ab93e0
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-05-23 00:49:41 +0900

    Solve: 기차와 파리 (JAVA)

---
+++ java/4849_기차와_파리/4849_기차와_파리.java
@@ -0,0 +1,31 @@
+// Problem: 기차와 파리
+// Number: 4849
+// Solved: 2026. 5. 23.
+// Language: Java
+
+// don't place package name.
+
+import java.io.*;
+import java.util.*;
+
+// don't change 'Main' class name and  'public' accessor.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long X = Long.parseLong(st.nextToken());
+        long Y = Long.parseLong(st.nextToken());
+        long Z = Long.parseLong(st.nextToken());
+
+        // 충돌까지 걸리는 시간 
+        double t = (double) X / (2.0 * Y);
+
+        // 파리가 이동한 거리 
+        double distance = Z * t;
+
+        // 소수점 버리기
+        System.out.println((long) distance);
+    }
+}
