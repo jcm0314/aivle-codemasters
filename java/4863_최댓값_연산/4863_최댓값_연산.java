c28304f41c92fb7acd43c625c9bd4c86548eb66b
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-05-28 17:30:40 +0900

    Solve: 최댓값 연산 (JAVA)

---
+++ java/4863_최댓값_연산/4863_최댓값_연산.java
@@ -0,0 +1,30 @@
+// Problem: 최댓값 연산
+// Number: 4863
+// Solved: 2026. 5. 28.
+// Language: Java
+
+// don't place package name.
+import java.io.*;
+import java.util.*;
+
+// don't change 'Main' class name and 'public' accessor.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+
+        int[] arr = {x, y, z};
+        Arrays.sort(arr); // arr[0] <= arr[1] <= arr[2]
+
+        // 최댓값이 최소 두 번 이상 등장해야 함
+        if (arr[1] == arr[2]) {
+            System.out.println("possible");
+        } else {
+            System.out.println("impossible");
+        }
+    }
+}
