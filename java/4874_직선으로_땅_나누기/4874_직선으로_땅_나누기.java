df4522f7c46d96342eba86d23a657579e861a36c
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-06-12 14:53:03 +0900

    Solve: 직선으로 땅 나누기 (JAVA)

---
+++ java/4874_직선으로_땅_나누기/4874_직선으로_땅_나누기.java
@@ -0,0 +1,27 @@
+// Problem: 직선으로 땅 나누기
+// Number: 4874
+// Solved: 2026. 6. 12.
+// Language: Java
+
+// don't place package name.
+import java.io.*;
+import java.util.*;
+
+// don't change 'Main' class name and 'public' accessor.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        int L = 0;
+        while (true) {
+            long regions = 1L + (long)L * (L + 1) / 2; // R(L)
+            if (regions >= N) {
+                System.out.println(L);
+                break;
+            }
+            L++;
+        }
+    }
+}
