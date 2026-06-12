d67370be160d8f732d432cefa2ff9bbf9155c421
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-06-12 13:27:03 +0900

    Solve: 신소재 개발 (JAVA)

---
+++ java/4895_신소재_개발/4895_신소재_개발.java
@@ -0,0 +1,29 @@
+// Problem: 신소재 개발
+// Number: 4895
+// Solved: 2026. 6. 12.
+// Language: Java
+
+// don't place package name.
+import java.io.*;
+import java.util.*;
+
+// don't change 'Main' class name and 'public' accessor.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        long total = A + B + C;
+        long maxCnt = Math.max(A, Math.max(B, C));
+        long rest = total - maxCnt;
+
+        if (maxCnt <= rest + 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
