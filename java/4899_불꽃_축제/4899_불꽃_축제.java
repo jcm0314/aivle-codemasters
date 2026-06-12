ebe4392cb1899881fb51883c01425cc2c4ee3cab
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-06-12 13:18:49 +0900

    Solve: 불꽃 축제 (JAVA)

---
+++ java/4899_불꽃_축제/4899_불꽃_축제.java
@@ -0,0 +1,33 @@
+// Problem: 불꽃 축제
+// Number: 4899
+// Solved: 2026. 6. 12.
+// Language: Java
+
+// don't place package name.
+import java.io.*;
+import java.util.*;
+
+// don't change 'Main' class name and 'public' accessor.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] a = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        // Kadane's algorithm
+        int maxEndingHere = a[0];  // dp[0]
+        int maxSoFar = a[0];
+
+        for (int i = 1; i < N; i++) {
+            maxEndingHere = Math.max(a[i], maxEndingHere + a[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        System.out.println(maxSoFar);
+    }
+}
